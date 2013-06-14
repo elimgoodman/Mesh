c7dd58dc942627905add2478de3e49ec04245697
Eli Goodman <eli.m.goodman@gmail.com>
2013-06-14 08:33:02 -0400

    structre in code, reflected on screen

--- static/js/index.js
+++ static/js/index.js
@@ -8,7 +8,12 @@ $(function(){
 
     App.module("Models", function(Models, App, Backbone, Marionette, $, _){
 
-        Models.StatementNode = Backbone.Model.extend({});
+        Models.StatementNode = Backbone.Model.extend({
+            defaults: {
+                type: null,
+                value: ""
+            }
+        });
 
         Models.StatementNodes = Backbone.Collection.extend({
             model: Models.StatementNode
@@ -16,7 +21,8 @@ $(function(){
 
         Models.Statement = Backbone.Model.extend({
             defaults: {
-                nodes: new Models.StatementNodes()
+                nodes: new Models.StatementNodes(),
+                type: null
             }
         });
 
@@ -30,9 +36,33 @@ $(function(){
     });
 
     App.module("Views", function(Views, App, Backbone, Marionette, $, _){
-    	Views.Statement = Backbone.Marionette.ItemView.extend({
+
+        Views.StatementNode = Backbone.Marionette.ItemView.extend({
+            template: "#statement-node-tmpl",
+            templateHelpers: {
+                getValue: function() {
+                    if(this.type == "ASSIGN") {
+                        return "->";
+                    } else if(this.type == "PRINT") {
+                        return "print";
+                    }
+                    else {
+                        return this.value;
+                    }
+                }
+            },
+            tagName: 'li',
+            className: 'statement-node'
+        });
+
+    	Views.Statement = Backbone.Marionette.CompositeView.extend({
     		template: "#statement-tmpl",
-            className: "statement"
+            className: "statement",
+            itemViewContainer: ".nodes",
+            itemView: Views.StatementNode,
+            initialize: function() {
+                this.collection = this.model.get('nodes');
+            }
     	});
 
     	Views.Statements = Backbone.Marionette.CollectionView.extend({
@@ -40,17 +70,45 @@ $(function(){
     	});
     });
 
-    App.addInitializer(function(options){
+    var createTestData = function() {
         var nodes = new App.Models.StatementNodes();
 
-        nodes.push(new App.Models.StatementNode());
-        nodes.push(new App.Models.StatementNode());
-        nodes.push(new App.Models.StatementNode());
+        nodes.push(new App.Models.StatementNode({
+            type: "INT",
+            value: "1"
+        }));
+        nodes.push(new App.Models.StatementNode({
+            type: "ASSIGN",
+        }));
+        nodes.push(new App.Models.StatementNode({
+            type: "SYMBOL",
+            value: "a"
+        }));
 
-    	App.Singletons.Statements.push(new App.Models.Statement({
+        var print_nodes = new App.Models.StatementNodes();
+
+        print_nodes.push(new App.Models.StatementNode({
+            type: "PRINT"
+        }));
+
+        print_nodes.push(new App.Models.StatementNode({
+            type: "SYMBOL",
+            value: "a"
+        }));
+
+        App.Singletons.Statements.push(new App.Models.Statement({
+            type: "ASSIGN",
             nodes: nodes
         }));
 
+        App.Singletons.Statements.push(new App.Models.Statement({
+            type: "PRINT",
+            nodes: print_nodes
+        }));
+    }
+
+    App.addInitializer(function(options){
+        createTestData();
     	var v = new App.Views.Statements({
     		collection: App.Singletons.Statements
     	});
