9a5a9cdb06cc5580e0e29293d95f2cdfc23d87f9
Dan McCormick <dan@codeop.com>
2013-06-24 22:10:28 -0400

    allow direct printing of integers; better exceptions

--- interpreter.py
+++ interpreter.py
@@ -1,17 +1,35 @@
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    def __init__(self, interpreter):
+        self.interpreter = interpreter
+
+class UndefinedSymbolError(Error):
+    def __init__(self, interpreter, symbol):
+        Error.__init__(self, interpreter)
+        interpreter.add_error('Unknown symbol: ' + symbol)
+
+class UnhandledMethodError(Error):
+    """For functionality that we don't know how to handle"""
+    def __init__(self, interpreter, method_name, error):
+        Error.__init__(self, interpreter)
+        interpreter.add_error('In method ' + method_name + ' I don\'t know how to handle ' + error)
+
 class Interpreter:
 
     def __init__(self):
         self.symbol_table = {}
-        self.stdout = ""
+        self.output = ""
         self.errors = []
+
+    def add_error(self, error):
+        self.errors.append(error)
 
     def get_symbol_value(self, symbol):
         try:
             value = self.symbol_table[symbol]
             return value
         except KeyError:
-            self.errors.append('Unknown symbol: ' + symbol)
-            raise
+            raise UndefinedSymbolError(self, symbol)
 
     def mesh_define(self, nodes):
         symbol = nodes[0]['value']
@@ -24,33 +42,43 @@ class Interpreter:
             self.mesh_print(nodes[1:])
 
     def mesh_print(self, nodes):
-        symbol = nodes[0]['value']
-        value  = self.get_symbol_value(symbol)
-        self.stdout += str(value)
+        node_type = nodes[0]['type']
+        if node_type == 'SYMBOL':
+            symbol = nodes[0]['value']
+            value  = self.get_symbol_value(symbol)
+        elif node_type == 'INT':
+            value = nodes[0]['value']
+        else:
+            raise UnhandledMethodError(self, "mesh_print", "a node_type of " + node_type)
+        self.output += str(value)
 
     def interpret(self, code):
         statements = code['statements']
 
         for statement in statements:
-            type = statement['type']
+            statement_type = statement['type']
             nodes = statement['nodes']
 
             try:
-                if type == 'DEFINE':
+                if statement_type == 'DEFINE':
                     self.mesh_define(nodes)
-                elif type == 'MUTATE':
+                elif statement_type == 'MUTATE':
                     self.mesh_mutate(nodes)
+                else:
+                    raise UnhandledMethodError(self, "main", "a statement type of " + statement_type)
 
                 response = {
                     'success': True,
-                    'stdout': self.stdout,
+                    'stdout': self.output,
                     'errors': []
                 }
-            except:
+            except (UndefinedSymbolError, UnhandledMethodError):
                 response = {
                     'success': False,
                     'stdout': '',
                     'errors': self.errors
                 }
+            except:
+                raise
 
         return response
--- interpreter_test.py
+++ interpreter_test.py
@@ -3,6 +3,15 @@ import json
 from interpreter import Interpreter
 
 class TestInterpreter(unittest.TestCase):
+    def test_print(self):
+        data='{"statements":[{"type":"MUTATE","nodes":[{"type":"SYMBOL","value":"print"},{"type":"INT","value":5}]}]}'
+        interpreter = Interpreter();
+        result = interpreter.interpret(json.loads(data))
+
+        self.assertTrue(result['success'])
+        self.assertEqual(result['stdout'], '5')
+        self.assertEqual(result['errors'], [])
+
     def test_assign_and_mutate(self):
         data='{"statements":[{"type":"DEFINE","nodes":[{"type":"SYMBOL","value":"a"},{"type":"INT","value":"1"}]},{"type":"MUTATE","nodes":[{"type":"SYMBOL","value":"print"},{"type":"SYMBOL","value":"a"}]}]}'
         interpreter = Interpreter();
@@ -20,6 +29,24 @@ class TestInterpreter(unittest.TestCase):
         self.assertFalse(result['success'])
         self.assertEqual(result['stdout'], '')
         self.assertEqual(result['errors'], ['Unknown symbol: a'])
+
+    def test_unhandled_method_in_print(self):
+        data='{"statements":[{"type":"MUTATE","nodes":[{"type":"SYMBOL","value":"print"},{"type":"SOMETHING_FUNKY","value":5}]}]}'
+        interpreter = Interpreter();
+        result = interpreter.interpret(json.loads(data))
+
+        self.assertFalse(result['success'])
+        self.assertEqual(result['stdout'], '')
+        self.assertEqual(result['errors'], ["In method mesh_print I don't know how to handle a node_type of SOMETHING_FUNKY"])
+
+    def test_unhandled_statement_type(self):
+        data='{"statements":[{"type":"WHOA_THERE", "nodes":[]}]}'
+        interpreter = Interpreter();
+        result = interpreter.interpret(json.loads(data))
+
+        self.assertFalse(result['success'])
+        self.assertEqual(result['stdout'], '')
+        self.assertEqual(result['errors'], ["In method main I don't know how to handle a statement type of WHOA_THERE"])
 
 if __name__ == '__main__':
     unittest.main()
